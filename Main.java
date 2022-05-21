66620752c108f1120dfb0c7aaf91a784154ab453
The Gecko <fatma.jadoon7@gmail.com>
2022-05-20 22:30:11 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,35 @@
  * Task distribution:
  * <ul>
  *   <li> Alexandra Mitnik
- * 
+ *      <ul>
+ *          <li> Total time spent: 10 hours
+ *          <li> Tasks:
+ *              <ul>
+ *                  <li> Designing and setting up Iradia's blog
+ *                  <li> Coding the 'esc' key function
+ *                  <li> Learning how to use Java FX
+ *              </ul>
+ *      </ul>
+ *   <li> Bethany Lum
+ *      <ul>
+ *          <li> Total time spent: 7 hours
+ *          <li> Tasks:
+ *              <ul>
+ *                  <li> Illustrating the character assets
+ *                  <li> Learning how to use digital art software
+ *              </ul>
+ *      </ul>
+ *   <li> Fatma Jadoon
+ *      <ul>
+ *          <li> Total time spent: 13 hours
+ *          <li> Tasks:
+ *              <ul>
+ *                  <li> Illustrating the background assets + logo
+ *                  <li> Designing blog
+ *                  <li> Coding the menu and splash screens
+ *                  <li> Learning how to use Java FX
+ *              </ul>
+ *      </ul>
  * </ul>
  */ 
 
