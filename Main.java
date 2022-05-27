e780ff425646ab770ba093b26995f1c6681cbe9c
The Gecko <fatma.jadoon7@gmail.com>
2022-05-27 10:57:32 -0400

    stuff

--- Main.java
+++ Main.java
@@ -1,11 +1,46 @@
 /**
- * This is the first draft of the Main class.
+ * This is the second draft of the Main class.
  * It creates an instance of and runs the game.
  * Version date: 05/20/2022
  * @author Fatma Jadoon
- * version: 1.0.0
+ * version: 1.2.27
  * <p>
- * First version submission of Iradia.
+ * Second version submission of Iradia.
+ * <p>
+ * Task distribution:
+ * <ul>
+ *   <li> Alexandra Mitnik
+ *      <ul>
+ *          <li> Total time spent: 8 hours
+ *          <li> Tasks:
+ *              <ul>
+ *                  <li>Revamping Iradia's blog
+ *                  <li>Connecting stage 1 and stage 2 together
+ *                  <li>Coding stage 2
+ *              </ul>
+ *      </ul>
+ *   <li> Bethany Lum
+ *      <ul>
+ *          <li> Total time spent: 7 hours
+ *          <li> Tasks:
+ *              <ul>
+ *                  <li>Illustrating the character assets
+ *                  <li>Learning how to use digital art software
+ *              </ul>
+ *      </ul>
+ *   <li> Fatma Jadoon
+ *      <ul>
+ *          <li> Total time spent: 13 hours
+ *          <li> Tasks:
+ *              <ul>
+ *                  <li>Illustrating the background assets + logo
+ *                  <li>Designing blog
+ *                  <li>Coding the menu and splash screens
+ *                  <li>Learning how to use Java FX
+ *              </ul>
+ *      </ul>
+ * </ul>
+ *
  * <p>
  * Task distribution:
  * <ul>
